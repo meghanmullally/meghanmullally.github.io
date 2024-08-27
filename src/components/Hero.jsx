05c30297e9feca6b07e9dbc32d6af8860c5ca1e2
Meghan Mullally <ms.mullally27@gmail.com>
2024-08-27 12:39:40 -0700

    set up initial template

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section id="hero">
+      <h1>Hi, I'm Meghan Mullally</h1>
+      <p>A passionate front-end developer.</p>
+    </section>
+  );
+}
+
+export default Hero;
