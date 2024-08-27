2157200a0a34b53cfd8c079a2eba50fbba0ed474
meghanmullally <ms.mullally27@gmail.com>
2024-08-27 14:22:10 -0700

    init styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Hero.module.css';
 
 function Hero() {
   return (
-    <section id="hero">
+    <section className="hero">
       <h1>Hi, I'm Meghan Mullally</h1>
-      <p>A passionate front-end developer.</p>
+      <p>A passionate front-end developer base in Seattle, Washington</p>
     </section>
   );
 }
