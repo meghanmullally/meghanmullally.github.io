9bbabae59ddf03ec84d5d02138ac52126341fda5
Meghan Mullally <ms.mullally27@gmail.com>
2024-08-27 13:29:16 -0700

    move files to folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import ProjectSection from './components/ProjectSection';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import ProjectSection from './components/ProjectSection/ProjectSection';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
