bf569996ca4d824a889c970cd288a4b267bee80b
meghanmullally <ms.mullally27@gmail.com>
2024-08-28 11:10:03 -0700

    add about me text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,9 @@ function About() {
     <section className={styles.about}>
       <h2>About Me</h2>
       <div className={styles['about-content']}>
-        <p>Hello, I'm Meghan, a tech professional with a diverse skill set spanning web development and technical support...</p>
+        <p> Hello, I'm Meghan, a tech professional with a diverse skill set spanning web development and technical support. 
+          With a background in both software engineering and customer-centric technical roles, I bring a unique blend of problem-solving and development skills to the table. 
+          Whether it's crafting seamless web applications or providing top-tier support.</p>
       </div>
     </section>
   );
