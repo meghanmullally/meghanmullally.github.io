99e0660d436ce432a7c88aaa0dd7057ab30ef432
meghanmullally <ms.mullally27@gmail.com>
2024-09-01 18:30:09 -0700

    added a contact form ,updated styling and added new colors green

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,92 @@
-import React from 'react';
-import styles from './Contact.module.css';
+import React, { useState } from "react";
+import styles from "./Contact.module.css";
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <section className={styles.contact} id="contact">
       <h2>Contact Me</h2>
-      <p>Email: <a href="mailto:meghan.mullallyjobs@gmail.com">meghan.mullallyjobs@gmail.com</a></p>
-      <p>GitHub: <a href="https://github.com/meghanmullally">meghanmullally</a></p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/meghan-mullally/">meghanmullally</a></p>
+      <div className={styles.contactContainer}>
+        <form id="contactForm" onSubmit={handleSubmit}>
+          <h3>Name (Required)</h3>
+          <div className={styles.formRow}>
+            <div className={styles.formGroup}>
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <label htmlFor="email">Email Address (Required):</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="subject">Subject (Required):</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="message">Message (Required):</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Enter your message here…"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+
+          <button type="submit">Send</button>
+        </form>
+        <div id="formOutput"></div>
+      </div>
     </section>
   );
 }
