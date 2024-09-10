d02f65d53db2a182c75847cf0dea9755b03b9402
meghanmullally <ms.mullally27@gmail.com>
2024-09-09 21:17:58 -0700

    add mui theme, create theme file and folder

---
+++ src/theme/theme.js
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#5e6c5b", // forest green shade
+            contrastText: "#fefcf6", // cloud white shade for text on primary
+        },
+        secondary: {
+            main: "#162a2c", // dark shade
+            contrastText: "#fefcf6", // cloud white shade for text on secondary
+        },
+        background: {
+            default: "#fefcf6", // cloud white shade for default background
+        },
+        text: {
+            primary: "#162a2c", // midnight shade for primary text
+            secondary: "#5e6c5b", // forest green shade for secondary text
+        },
+        button: {
+            primary: "#5e6c5b",
+            secondary: "#fefcf6",
+        }
+    },
+});
+
+export default theme;
